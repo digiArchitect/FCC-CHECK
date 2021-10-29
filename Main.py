6979aa09021329cbac9aa7f1df1bf407b025b0fe
digiArchitect <66040569+digiArchitect@users.noreply.github.com>
2021-10-28 21:01:46 -0400

    Update Main.py
    Added a function which returns a list of words that are in the lyrics

--- Main.py
+++ Main.py
@@ -1,12 +1,12 @@
 #Steps!
 #                       Part One!
-#1. Given a word check whether or not it equals a bad word   
-#2. Given a list of lyrics, return whether or not they have certain banned words
-#2b. Lyrics in this case is defined as a list of strings
+#1. Given a word check whether or not it equals a bad word done!
+#2. Given a list of lyrics, return whether or not they have certain banned words done!
+#2b. Lyrics in this case is defined as a list of strings 
 #            _______________________________________________________
 #                       Part Two!
-#1. Given a word and a list of bad words, return the bad word it is equal too (if applicabble)
-#2. Given lyrics and a list of bad words, return all the bad words within the lyrics (if applicable)
+#1. Given lyrics and a list of bad words, return all the bad words within the lyrics (if applicable) done!
+#2. Create an acceptable list of lyrics
 #           ________________________________________________________
 
 
@@ -27,14 +27,18 @@ lyrics1 = ['Hey,', 'I', 'just', 'met', 'you', 'and', 'this', 'is', 'crazy']
 lyrics2 = ['Hey,', 'I', 'just', 'met', 'you', 'and', 'this', 'is', 'crazy', 'fuck']
 #has_expletive: |List-of Strings||List-of Strings| -> Boolean
 # Given lyrics and a list of banned words, returns whether or not there are any curse words.
-def has_expletive(lyrics, banned):
+def has_expletive(lyrics, banned): #Very strong feeling that this could be optimized further
     for x in lyrics:
        if expletive(x, banned):
            return True
     return False
 print("This should return false: " + str(has_expletive(lyrics1,bannedwords))) # Value is false
 print("This should return true: " + str(has_expletive(lyrics2,bannedwords))) # Value is true 
-    
+#list_of_expletives: |List-of Strings||List-of Strings| -> |List-of Strings|  
+def list_of_expletives(lyrics,banned):
+    return(list((filter (lambda x: expletive(x,banned), lyrics))))
+print (list_of_expletives(lyrics1,bannedwords))
+print (list_of_expletives(lyrics2,bannedwords))
 
 
 
