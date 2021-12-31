287ce11d86b5e498abe5d1d700bb3c44bceff166
Jylah <66040569+digiArchitect@users.noreply.github.com>
2021-12-31 21:18:38 +0000

    Optimized Code reduced redundancy in redefining bannedwords

--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
+
+
 #Steps!
 #                       Part One!
 #1. Given a word check whether or not it equals a bad word done!
@@ -7,38 +9,58 @@
 #                       Part Two!
 #1. Given lyrics and a list of bad words, return all the bad words within the lyrics (if applicable) done!
 #2. Create an acceptable list of lyrics
-#           ________________________________________________________
+#           ______________________________________________________
+# Part Three!
+#1. Add Genius Capability which fetches lyrics from a song and analyzes if it has unnacetable words
+# Part Four!
+#1. Add Spotify and Youtube capability 
+#2. Takes the song from spotify/youtube, fetches lyrics using part 3 and then produces if its acceptable
+# Part Five!
+#1. Make this work with playlists
 
-
+from lyricsgenius import Genius
+import os
+token = os.environ['token']
+genius = Genius(token)
 # List of common banned words by FCC standards
-bannedwords = ["shit", "piss", "fuck", "cunt", "cocksucker", "motherfucker", "tits", "weed", "beer", "damn", "bitch"]
+banned = ["shit", "piss", "fuck", "cunt", "cocksucker", "motherfucker", "tits", "weed", "beer", "sake", "damn", "bitch"]
 #Two example words one which is banned the other which is not
 exWord1 = "hey" 
 exWord2 = "shit"
 # bad_word: String List-of Strings -> Boolean
 #Given a word checks whether or not it is a bad word
-def expletive(word, banned):
+def expletive(word):
    return word in banned
-print("This should return false: " + str(expletive(exWord1, bannedwords))) #returns false
-print("This should return true: " + str(expletive(exWord2, bannedwords))) #return true
+print("This should return false: " + str(expletive(exWord1))) #returns false
+print("This should return true: " + str(expletive(exWord2))) #return true
 
 #Two versions of the same lyrics, one of which contains an expletive and one of which does not
 lyrics1 = ['Hey,', 'I', 'just', 'met', 'you', 'and', 'this', 'is', 'crazy']
 lyrics2 = ['Hey,', 'I', 'just', 'met', 'you', 'and', 'this', 'is', 'crazy', 'fuck']
 #has_expletive: |List-of Strings||List-of Strings| -> Boolean
 # Given lyrics and a list of banned words, returns whether or not there are any curse words.
-def has_expletive(lyrics, banned): #Very strong feeling that this could be optimized further
+def has_expletive(lyrics): #Very strong feeling that this could be optimized further
     for x in lyrics:
-       if expletive(x, banned):
+       if expletive(x):
            return True
     return False
-print("This should return false: " + str(has_expletive(lyrics1,bannedwords))) # Value is false
-print("This should return true: " + str(has_expletive(lyrics2,bannedwords))) # Value is true 
-#list_of_expletives: |List-of Strings||List-of Strings| -> |List-of Strings|  
-def list_of_expletives(lyrics,banned):
-    return(list((filter (lambda x: expletive(x,banned), lyrics))))
-print (list_of_expletives(lyrics1,bannedwords))
-print (list_of_expletives(lyrics2,bannedwords))
+print("This should return false: " + str(has_expletive(lyrics1))) # Value is false
+print("This should return true: " + str(has_expletive(lyrics2))) # Value is true 
+#list_of_expletives: |List-of Strings|-> |List-of Strings|  
+def list_of_expletives(lyrics):
+    return(list((filter (lambda x: expletive(x), lyrics))))
+print (list_of_expletives(lyrics1))
+print (list_of_expletives(lyrics2))
+
+#MaybeString:
+  #String
+  #False
+#def checkWords(song): #Returns MaybeString
+  
+  # if (list_of_expletives(lyrics,banned) ==
+
+ # return False
+
 
 
 
