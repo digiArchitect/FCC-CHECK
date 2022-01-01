84284a056cfe053f7cd35577fbdd0a578a52b3df
Jylah <66040569+digiArchitect@users.noreply.github.com>
2022-01-01 19:51:21 +0000

    Imported regex as a means to get exact string.

--- Supplemental.py
+++ Supplemental.py
@@ -1,16 +1,19 @@
 import os
 from lyricsgenius import Genius
+import re
+banned = ''
+with open('banned.txt') as f:
+     banned = f.read().splitlines()
 token = os.environ['token']
 genius = Genius(token)
 def get_lyrics(artist,title):
   lyrics = genius.search_song(title, artist).lyrics
   return lyrics
+def has_expletive(word,lyrics): #Very strong feeling that this
+  if re.search(r'\b' + word + r'\b', lyrics):
+    return True
+  return False
   
-
-banned = ["shit", "piss", "fuck", "cunt", "cocksucker", "motherfucker", "tits", "weed", "beer", "sake", "damn", "bitch"]
-def has_expletive(word,lyrics): #Very strong feeling that this 
-  if (lyrics.find(word) == -1):
-    return False
-  return True 
 def expletives(lyrics):
     return(list((filter (lambda x: has_expletive(x,lyrics), banned))))
+
