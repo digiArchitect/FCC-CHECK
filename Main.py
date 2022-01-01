b0da91e3c0853ab39019de7195750a4eaa7f69ef
digiArchitect <66040569+digiArchitect@users.noreply.github.com>
2022-01-01 16:17:38 -0500

    Final version of Part Three
    Cleaned up

--- Main.py
+++ Main.py
@@ -1,23 +1,14 @@
 from Supplemental import *
+
+
 def song():
-  name = input("Artist: ")
-  title = input("Song: ")
-  lyrics = get_lyrics(name,title)
-  badWords = ", ".join(expletives(lyrics))
-  if not badWords:
-    return "This song is acceptable on the air waves"
-
-  return f"Due to word(s) {badWords} this song is unacceptable on the air waves"   
-   
-
-print (song())
+    name = input("Artist: ")
+    title = input("Song: ")
+    lyrics = get_lyrics(name, title)
+    bad_words = ", ".join(expletives(lyrics))
+    if not bad_words:
+        return "This song is acceptable on the air waves"
+    return f"Due to word(s) {bad_words} this song is unacceptable on the air waves"
 
 
-
-
-
-
-
-
-    
-    
+print(song())
--- Supplemental.py
+++ Supplemental.py
@@ -1,18 +1,25 @@
 import os
-from lyricsgenius import Genius
 import re
-banned = ''
+from lyricsgenius import Genius
+from dotenv import load_dotenv
+load_dotenv()
+
 with open('banned.txt') as f:
-     banned = f.read().splitlines()
+    banned = f.read().splitlines()
 token = os.environ['token']
 genius = Genius(token)
-def get_lyrics(artist,title):
-  lyrics = genius.search_song(title, artist).lyrics
-  return lyrics
-def has_expletive(word,lyrics): #Very strong feeling that this
-  if re.search(r'\b' + word + r'\b', lyrics):
-    return True
-  return False 
-def expletives(lyrics):
-    return(list((filter (lambda x: has_expletive(x,lyrics), banned))))
 
+
+def get_lyrics(artist, title):
+    lyrics = genius.search_song(title, artist).lyrics
+    return lyrics
+
+
+def has_expletive(word, lyrics):
+    if re.search(r'\b' + word + r'\b', lyrics):
+        return True
+    return False
+
+
+def expletives(lyrics):
+    return list((filter(lambda x: has_expletive(x, lyrics), banned)))
