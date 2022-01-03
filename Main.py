ab5fdadd635b488d00478edd5f35e6eb00081668
digiArchitect <66040569+digiArchitect@users.noreply.github.com>
2022-01-03 01:33:49 -0500

    Updated print statement
    Changed up print statement and set up for playlist

--- Main.py
+++ Main.py
@@ -1,10 +1,26 @@
 from Supplemental import *
+def main():
+    print("Hello this is FCC Checker!")
+    print("Would you like to check a song or a playlist?")
+    option = input("Type S for Song and P for Playlist: ").lower()
+    if option == "s":
+        print(song())
+    elif option == "p":
+        print(playlist())
+    else:
+        print(f' Error: Unknown option "{option}"')
+
 
 def song():
     song = input("Spotify Song link: ")
-    lyrics = get_lyrics(song)
-    bad_words = ", ".join(expletives(lyrics))
+    dict = get_values(song)
+    title = dict["title"]
+    artist = dict["artist"]
+    bad_words = ", ".join(expletives(dict["lyrics"]))
     if not bad_words:
-        return "This song is acceptable on the air waves"
-    return f"Due to word(s) {bad_words} this song is unacceptable on the air waves"
-print(song())
+        return f"{title} by {artist} is acceptable on the air waves"
+    return f"{title} by {artist}  is unacceptable on the air waves due to word(s) {bad_words}"
+
+def playlist():
+    print("hey this is here!")
+main()
